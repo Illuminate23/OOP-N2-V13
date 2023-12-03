808eb4c49358ac86b4bb05a200950525006aed10
Illuminate23 <midas-2019@mail.ru>
2023-12-04 00:48:52 +0600

    commit

--- BracketChecker.java
+++ BracketChecker.java
@@ -2,31 +2,41 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BracketChecker {
+public class BracketChecker 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите выражение для проверки скобок:");
         String expression = scanner.nextLine();
 
-        if (isBalanced(expression)) {
-            System.out.println("Выражение правильное.");
-        } else {
-            System.out.println("Выражение неправильное.");
+        if (isBalanced(expression)) 
+        {
+            System.out.println("Выражение правильное");
+        } 
+        else 
+        {
+            System.out.println("Выражение неправильное");
         }
 
         scanner.close();
     }
 
-    private static boolean isBalanced(String expression) {
+    private static boolean isBalanced(String expression) 
+    {
         int openBrackets = 0, closeBrackets = 0;
         Pattern pattern = Pattern.compile("[()]");
         Matcher matcher = pattern.matcher(expression);
 
-        while (matcher.find()) {
-            if (matcher.group().equals("(")) {
+        while (matcher.find()) 
+        {
+            if (matcher.group().equals("(")) 
+            {
                 openBrackets++;
-            } else {
+            } 
+            else 
+            {
                 closeBrackets++;
             }
         }
